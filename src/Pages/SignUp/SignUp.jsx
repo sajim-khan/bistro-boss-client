db2bbec60fed4ad56be6d37d0df708e083ce7396
sajim-khan <shossain1427@gmail.com>
2023-06-02 23:10:15 +0600

    added react hook form validation

--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -1,4 +1,16 @@
+import { useForm } from "react-hook-form";
+
 const SignUp = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
     <div className="hero min-h-screen bg-base-200">
       <div className="hero-content flex-col lg:flex-row-reverse">
@@ -11,26 +23,51 @@ const SignUp = () => {
           </p>
         </div>
         <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-          <div className="card-body">
+          <form onSubmit={handleSubmit(onSubmit)} className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Name</span>
+              </label>
+              <input
+                type="text"
+                placeholder="Name"
+                {...register("name", { required: true })}
+                name="name"
+                className="input input-bordered"
+              />
+              {errors.name && <span>Name is required</span>}
+            </div>
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Email</span>
               </label>
               <input
                 type="text"
+                {...register("email", { required: true })}
+                name="email"
                 placeholder="email"
                 className="input input-bordered"
               />
+              {errors.email && <span>Email is required</span>}
             </div>
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Password</span>
               </label>
               <input
-                type="text"
+                type="password"
                 placeholder="password"
+                {...register("password", {
+                  required: true,
+                  minLength: 6,
+                  maxLength: 15,
+                })}
+                name="password"
                 className="input input-bordered"
               />
+              {errors.password && (
+                <span>Password is required, minLength 6 and maxLength 15</span>
+              )}
               <label className="label">
                 <a href="#" className="label-text-alt link link-hover">
                   Forgot password?
@@ -40,7 +77,7 @@ const SignUp = () => {
             <div className="form-control mt-6">
               <button className="btn btn-primary">SignUp</button>
             </div>
-          </div>
+          </form>
         </div>
       </div>
     </div>
