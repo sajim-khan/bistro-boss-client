39ee0342a9fcecde4c44274b4e6dbcc6a345eaae
sajim-khan <shossain1427@gmail.com>
2023-06-04 10:45:30 +0600

    added functionality for post method

--- src/Components/FoodCard/FoodCard.jsx
+++ src/Components/FoodCard/FoodCard.jsx
@@ -1,7 +1,45 @@
+import { useContext } from "react";
+import { AuthContext } from "../../Provider/AuthProvider";
+import Swal from "sweetalert2";
+import { useNavigate } from "react-router-dom";
+
 const FoodCard = ({ item }) => {
   const { image, name, price } = item;
+  
+  const {user} = useContext(AuthContext)
+  const navigate = useNavigate()
+  
   const handleAddToCart = item => {
     console.log(item);
+    if(user){
+      fetch("http://localhost:5000/carts")
+      .then(res => res.json())
+      .then(data => {
+        if(data.insertedId){
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: "User created successfully",
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        }
+      })
+    }
+    else {
+      Swal.fire({
+        title: "Please login to order to food",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Login Now!",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          navigate('/login')
+        }
+      });
+    }
   }
 
   return (
